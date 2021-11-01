2a8028f5a0e43e529818f8447294fc697dbfec32
utkucaglayan <utku.caglayan@hazelcast.com>
2021-11-01 12:18:54 +0300

    hazelcast-clc 0.0.1 (new formula)

---
+++ Formula/hazelcast-commandline-client.rb
@@ -0,0 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class HazelcastCommandlineClient < Formula
+  desc "Home of Hazelcast Commandline Client Project"
+  homepage "https://github.com/hazelcast/hazelcast-commandline-client/tree/main"
+  url "https://github.com/hazelcast/hazelcast-commandline-client/archive/refs/tags/v1.0.0-test.6.tar.gz"
+  sha256 "4c3a6ce10516ccc4eaa1ccc13c5ca28da73905ac0f32750ee6a9de43ac9bdcd8"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test hazelcast-commandline-client`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
+  end
+end
