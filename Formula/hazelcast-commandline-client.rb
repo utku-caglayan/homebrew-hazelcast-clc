20c288908fd8d18c591e35de4ade5158a2a2e0fd
utkucaglayan <utku.caglayan@hazelcast.com>
2021-11-01 23:52:57 +0300

    refactor

--- Formula/hazelcast-commandline-client.rb
+++ Formula/hazelcast-commandline-client.rb
@@ -5,8 +5,6 @@ class HazelcastCommandlineClient < Formula
   sha256 "4c3a6ce10516ccc4eaa1ccc13c5ca28da73905ac0f32750ee6a9de43ac9bdcd8"
   license "Apache-2.0"
 
-  depends_on "go" => :build
-
   bottle do
     sha256 arm64_monterey: "01e1dc0704d6606839599ef423d34767997725d8e25110ce848e20434681efc5"
     sha256 arm64_big_sur:  "1adc0340427aeef3afbb0980b6fcea4edcfa6127ea44846615065b68a27544a8"
@@ -16,6 +14,8 @@ class HazelcastCommandlineClient < Formula
     sha256 mojave:         "7d17fffd0920dbfdd79c68c38f375fae4da7dbaf4965f8d6a2876bbb9bc87e84"
     sha256 x86_64_linux:   "a964c718912feab17767ba90a09d15d12c2bb3240709cf70b751f413f2d72d87"
   end
+
+  depends_on "go" => :build
 
   def install
     system "go", "build", "-o", "hzc", *std_go_args(ldflags: "-s -w")
