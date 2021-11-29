6d763d43c650347a257345a02e705c419646301b
Utku Çağlayan <utku.caglayan@hazelcast.com>
2021-11-29 14:36:17 +0300

    version increment

--- Formula/hazelcast-commandline-client.rb
+++ Formula/hazelcast-commandline-client.rb
@@ -1,15 +1,9 @@
 class HazelcastCommandlineClient < Formula
   desc "Home of Hazelcast Command-line Client Project"
   homepage "https://github.com/hazelcast/hazelcast-commandline-client/tree/main"
-  url "https://github.com/utku-caglayan/hazelcast-commandline-client/archive/refs/tags/v1.0.0-interactive-test.2.tar.gz"
+  url "https://github.com/hazelcast/hazelcast-commandline-client/archive/refs/tags/v1.0.0-test.7.tar.gz"
   sha256 "258ab61febac914429cad3f0b00f692228ec95543d5fafe43c9002094be60f66"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/utku-caglayan/homebrew-hazelcast-clc/releases/download/hazelcast-commandline-client-2"
-    sha256 cellar: :any_skip_relocation, catalina:     "36223067ef24929a128e9b37d89d59569fc65a1acdb1fc00c28e1f39b94f87d5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "41f3998410c6374d8f1bfec915216c2b379f7695445a0303017e5468ee9ebda7"
-  end
 
   depends_on "go" => :build
 
@@ -25,7 +19,8 @@ class HazelcastCommandlineClient < Formula
     opoo "See https://docs.brew.sh/Shell-Completion to enable autocompletion"
     ohai "Example usages:
 * `hzc map --name my-map put --key hello --value world` - put entry into map
-* `hzc --help` - print help"
+* `hzc --help` - print help
+* `hzc` - fire up an interactive shell"
   end
 
   test do
