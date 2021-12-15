9c6458b4f220a8530b9b72e60f6652e745460822
Utku Çağlayan <utku.caglayan@hazelcast.com>
2021-12-15 07:37:00 +0000

    hazelcast-commandline-client: add 9 bottle.

--- Formula/hazelcast-commandline-client.rb
+++ Formula/hazelcast-commandline-client.rb
@@ -4,6 +4,13 @@ class HazelcastCommandlineClient < Formula
   url "https://github.com/hazelcast/hazelcast-commandline-client/archive/refs/tags/v1.0.0-test.9.tar.gz"
   sha256 "53268981fdcd4e3459de406ee1ff82cb23bb5686fdae12278aaa49a64e137de0"
 
+  bottle do
+    root_url "https://github.com/utku-caglayan/homebrew-hazelcast-clc/releases/download/hazelcast-commandline-client-9"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, big_sur:      "cdfb6ef5a552a6cc8d469a3b58c67488654d8bbd2b6f12020ddb735857cda889"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "7fb1e3b0f5147c8080ad81847b21d70d0b6825b55ba2c30069bb5b10570aaf21"
+  end
+
   depends_on "go" => :build
 
   def install
